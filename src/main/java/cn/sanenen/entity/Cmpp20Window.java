e91a637c89fc9dae5d3887889f136f29d35cafa8
sun <sun@sanenen.cn>
2019-04-23 11:00:30 +0800

    新建 内容数据框事件类

---
+++ src/main/java/cn/sanenen/entity/Cmpp20Window.java
@@ -0,0 +1,5 @@
+package cn.sanenen.entity;
+
+public class Cmpp20Window {
+
+}
---
+++ src/main/java/cn/sanenen/listener/TextAreaContentListener.java
@@ -0,0 +1,18 @@
+package cn.sanenen.listener;
+
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+
+/**
+ * 内容数据框 事件
+ * @author sun
+ * 2019年4月23日 上午11:14:04
+ */
+public class TextAreaContentListener implements CaretListener {
+
+	@Override
+	public void caretUpdate(CaretEvent e) {
+		
+	}
+
+}
